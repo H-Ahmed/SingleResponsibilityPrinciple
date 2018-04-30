73bf626387001fb7cb2373e81c8b8a668f32769f
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-04-30 13:13:03 +0200

    Initial commit

---
+++ src/com/company/Book.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Book {
+
+    private int numOfPages;
+    private String authorName;
+
+    public Book(String authorName, int numOfPages) {
+        this.authorName = authorName;
+        this.numOfPages = numOfPages;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    //Not Single Responsibility
+    //=======================
+    public void printAndSave() {
+        System.out.println("Printing the book....");
+        System.out.println("Saving the book...");
+    }
+    //=======================
+
+    //Single Responsibility
+    //=======================
+    public void print() {
+        System.out.println("Printing the book....");
+    }
+
+    public void save() {
+        System.out.println("Saving the book...");
+    }
+    //=======================
+
+    @Override
+    public String toString() {
+        return authorName + " - " + numOfPages;
+    }
+}
